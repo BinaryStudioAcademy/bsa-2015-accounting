4e2d60bf51749d8c4832b5a4baa0d5d9f3ff5c64
voby <its.voby@gmail.com>
2015-08-05 18:05:59 +0300

    Auth task: changed bcrypt to bcryptjs for windows folks

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,6 @@
 var passport = require('passport'),
 LocalStrategy = require('passport-local').Strategy,
-bcrypt = require('bcrypt');
+bcrypt = require('bcryptjs');
 
 passport.serializeUser(function(user, done) {
     done(null, user.id);
