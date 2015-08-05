55af6ecd8395cced0096bee00db4f8e55e978a9d
voby <its.voby@gmail.com>
2015-08-05 17:25:53 +0300

    Auth task: added forms, auth via login-email and password

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,24 +6,6 @@
  */
 
 module.exports = {
-
-  // blueprint API
-  find: getUsers
+	
 };
-
-function getUsers(req, res) {
-  User.native(function(err, collection) {
-    if(err) return res.serverError(err);
-
-      collection.find({}, {
-        _id: true,
-        login: true,
-        name: true,
-        role: true
-      }).toArray(function (err, results) {
-        if(err) return res.serverError(err);
-        return res.send(results);
-      });
-  });
-}
 
--- api/models/User.js
+++ api/models/User.js
@@ -1,31 +1,42 @@
-/**
-* User.js
-*
-* @description :: TODO: You might write a short summary of how this model works and what it represents here.
-* @docs        :: http://sailsjs.org/#!documentation/models
-*/
+var bcrypt = require('bcrypt');
 
 module.exports = {
-  connection: 'mongo',
-  attributes: {
-    _id: {
-      type: 'string',
-      primaryKey: true,
-      required: true
+    attributes: {
+        login: {
+            type: 'email',
+            required: true,
+            unique: true
+        },
+        password: {
+            type: 'string',
+            minLength: 6,
+            required: true
+        },
+        name: {
+        	type: 'string',
+        	default: 'anonymous'
+        },
+        role: {
+		type: 'string',
+		default: 'manager'
+        },
+        toJSON: function() {
+            var obj = this.toObject();
+            delete obj.password;
+            return obj;
+        }
     },
-    name: {
-      type: 'string'
-    },
-    role: {
-      type: 'string'
-    },
-    login: {
-      type: 'string',
-      unique: true
-    },
-    password: {
-      type: 'string'
+    beforeCreate: function(user, cb) {
+        bcrypt.genSalt(10, function(err, salt) {
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                if (err) {
+                    console.log(err);
+                    cb(err);
+                } else {
+                    user.password = hash;
+                    cb();
+                }
+            });
+        });
     }
-  }
 };
-
