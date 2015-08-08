ac7baa75f5a104632795337d58eef63670e42f67
jacobator <jacobator@gmail.com>
2015-08-08 22:11:19 +0300

    Almost done

--- assets/js/budgets/Budgets.controller.js
+++ assets/js/budgets/Budgets.controller.js
@@ -12,11 +12,12 @@ module.exports = function(app) {
     vm.toggleCustom = function (index) {
         vm.hiddenList[index] = !vm.hiddenList[index];
     };
-		vm.expenses = [];
 
 		vm.rawBudgets = [];
+		vm.rawExpenses = [];
 
 		vm.budgets = [];
+		vm.expenses = [];
 
 		vm.categories = [];
 		//vm.year = new Date().getFullYear() + 1;
@@ -24,37 +25,67 @@ module.exports = function(app) {
 		//vm.years = [];
 
 		////////create route /years ???
-		BudgetsService.getBudgets().then(function(budgets) {
-			vm.rawBudgets = budgets;
-			budgets.forEach(function(budget) {
+		BudgetsService.getBudgets().then(function(rawBudgets) {
+			vm.rawBudgets = rawBudgets;
+			vm.rawBudgets.forEach(function(budget) {
 				if (vm.years.indexOf(String(budget.year)) < 0) vm.years.push(String(budget.year));
 			});
 
-			vm.year = vm.years[0];
-			vm.updateYear();
+			ExpensesService.getAllExpenses().then(function(rawExpenses) {
+				vm.rawExpenses = rawExpenses;
+				
+				vm.year = vm.years[0];
+				vm.updateYear();
+			});
+			
+
+
+
+			
 			//vm.years.unshift(+vm.years[0] + 1 + "");
 
-			//ExpensesService.getRequest(expensesIndex, expensesCount).then(function(expenses) {
-			//	vm.expenses = expenses;
-			//});
 		});
 
 		vm.updateYear = function() {
 			vm.budgets = _.filter(vm.rawBudgets, {year: Number(vm.year)});
 
-			vm.budgetSum = 0;
+			vm.expenses = _.filter(vm.rawExpenses, function(expense) {
+				return (new Date(expense.time * 1000)).getFullYear() == vm.year;
+			});
+
+			vm.annualBudget = 0;
+			vm.annualUsed = 0;
+
 			vm.categories = [];
 			vm.budgets.forEach(function(budget) {
-				vm.budgetSum += budget.budget;
+				vm.annualBudget += budget.budget;
 				var subcategories = [];
+
+				var catUsed = 0;
+
 				budget.subcategories.forEach(function(sub) {
 					var nameIndex = _.findIndex(budget.categoryId.subcategories, function(s) {
 						return s.id === sub.id;
 					});
-					var subcategory = {name: budget.categoryId.subcategories[nameIndex].name, budget: sub.budget}
-					subcategories.push(subcategory);
-				});
-				var category = {name: budget.categoryId.name, budget: budget.budget, subcategories: subcategories, used: 'USED*'};
+
+					var subExpenses = _.filter(vm.expenses, {subcategoryId: sub.id});
+					
+					
+					var subUsed = 0;
+
+					subExpenses.forEach(function(subExpense) {
+						subUsed += subExpense.price;
+					});
+
+					catUsed += subUsed;
+
+					var subcategory = {name: budget.categoryId.subcategories[nameIndex].name, budget: sub.budget, used: subUsed}
+						subcategories.push(subcategory);
+					});
+
+
+
+				var category = {name: budget.categoryId.name, budget: budget.budget, subcategories: subcategories, used: catUsed};
 				vm.categories.push(category);
 			});
 		};
