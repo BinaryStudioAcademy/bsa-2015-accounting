14663e81c86d4ee9e7bd4480016518dbac71f3f3
jacobator <jacobator@gmail.com>
2015-08-09 20:08:38 +0300

    Years route

---
+++ assets/js/shared/Years.service.js
@@ -0,0 +1,23 @@
+module.exports = function(app) {
+  app.factory('YearsService', YearsService);
+
+  YearsService.$inject = ["$resource"];
+
+  function YearsService($resource) {
+    return {
+      getYears: getYears
+    };
+
+    function getRequest() {
+      return $resource("/years");
+    }
+
+    /**
+     * Gets categories array
+     * @returns categories array
+     */
+    function getYears() {
+      return getRequest().query().$promise;
+    }
+  }
+};
