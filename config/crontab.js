92b518edbe78315b9c53124b5daafe57c988195d
PYYuriy <y.pavlyo@ukr.net>
2015-08-20 10:41:45 +0300

    rwquest crontab

--- config/crontab.js
+++ config/crontab.js
@@ -1,5 +1,8 @@
 module.exports.crontab = {
   '00 00 00 1-31 * *': function() {
     require('../crontab/currency.js').run();
-  }
+  },
+  ' * * 5 * *': function() {
+    require('../crontab/carrencyForPeriod.js').run();
+  },
 };
