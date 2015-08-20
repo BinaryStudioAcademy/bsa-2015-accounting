ee29f6c344bace2684d08db4c371bf84aee60510
jacobator <jacobator@gmail.com>
2015-08-20 12:56:52 +0300

    generator fixes

--- mock-data-generator.js
+++ mock-data-generator.js
@@ -6,6 +6,8 @@ var Factory = require('rosie').Factory;
 var _ = require('lodash');
 var bcrypt = require('bcryptjs');
 var MongoClient = require('mongodb').MongoClient;
+var objectId = require('./config/objectId.js');
+var ObjectId = require('mongodb').ObjectID;
 
 var startYear = 2009;
 var years = 7;
@@ -25,37 +27,38 @@ db.budget = [];
 db.expense = [];
 db.user = [];
 db.currency = [];
+db.history = [];
 
 Factory.define('Subcategory')
-	.sequence('id', function() {return String(casual.integer(0, 10000000));});
+	.sequence('id', function() {return objectId.ObjectId();});
 
 Factory.define('Category')
-	.sequence('_id', function() {return String(casual.integer(0, 10000000));})
+	.sequence('_id', function() {return new ObjectId();})
 	.attr('createdAt', function() {return new Date().toISOString();})
 	.attr('updatedAt', function() {return new Date().toISOString();});
 
 
 Factory.define('Budget')
-	.sequence('_id', function() {return String(casual.integer(0, 10000000));})
+	.sequence('_id', function() {return new ObjectId();})
 	.attr('createdAt', function() {return new Date().toISOString();})
 	.attr('updatedAt', function() {return new Date().toISOString();});
 
 Factory.define('Expense')
-	.sequence('_id', function() {return String(casual.integer(0, 10000000));})
-	.attr('price', function() {return casual.integer(3000, 10000);})
+	.sequence('_id', function() {return new ObjectId();})
+	.attr('price', function() {return casual.integer(500, 2000);})
 	.attr('currency', function() {return casual.random_element(['USD', 'UAH']);})
 	.attr('description', function() {return casual.description;})
 	.attr('personal', function() {return false;})
 	.attr('name', function() {return casual.random_element(events) + ' #' + casual.integer(1, 9) + ': ' + casual.random_element(charges);});
 
 Factory.define('Currency')
-	.sequence('_id', function() {return String(casual.integer(0, 10000000));})
+	.sequence('_id', function() {return new ObjectId();})
 	.attr('rate', function() {return Number((Math.random() * (25 - 16) + 16).toFixed(2))})
 	.attr('createdAt', function() {return new Date().toISOString();})
 	.attr('updatedAt', function() {return new Date().toISOString();});
 
 Factory.define('User')
-	.sequence('_id', function() {return String(casual.integer(0, 100000));})
+	.sequence('_id', function() {return new ObjectId();})
 	.sequence('login', function() {return casual.email})
 	.attr('name', function() {return casual.name})
 	.attr('createdAt', function() {return new Date().toISOString();})
@@ -69,14 +72,14 @@ _.times(1000, function() {
 _.times(categories.length, function(n) {
 	var subs = subcategories.splice(0, 8);
 	var managers = _.sample(db.user, 20).map(function(usr) {
-		return usr._id;
+		return String(usr._id);
 	});
 	var names = categories;
 	subs.map(function(s, i) {
 		return s.name = names[n] + '-sub-' + i;
 	});
 	var category = Factory.build('Category', {name: names[n], subcategories: subs});
-	var categoryId = category._id;
+	var categoryId = String(category._id);
 	_.times(5, function() {
 		var user = Factory.build('User', {password: hash, admin: false, categories: [{id: categoryId, budget: casual.integer(29, 33) * 1000, level: casual.integer(1, 3)}]});
 		db.user.push(user);
@@ -92,7 +95,7 @@ _.times(categories.length, function(n) {
 			return {id: sub.id, budget: money};
 		});
 		var budget_money = Math.round((total * casual.random_element([1, Number((Math.random() * (1.15 - 1) + 1).toFixed(2))])) / 1000 ) * 1000;
-		var budget = Factory.build('Budget', {year: startYear + n, category: {id: category._id, budget: budget_money}, subcategories: sub_with_money, creatorId: _.sample(managers, 1)[0]});
+		var budget = Factory.build('Budget', {year: startYear + n, category: {id: String(category._id), budget: budget_money}, subcategories: sub_with_money, creatorId: _.sample(managers, 1)[0]});
 		db.budget.push(budget);
 	});
 });
@@ -114,27 +117,29 @@ _.times(years, function(n) {
 					db.currency.push(currency);
 				}
 				if (nn === 4) {
-					var expense = Factory.build('Expense', {deletedBy: _.sample(db.user)._id, time: expTime, creatorId: String(_.sample(db.user)._id), categoryId: category._id, subcategoryId: sub.id});
-					db.expense.push(expense);
+					var expense = Factory.build('Expense', {deletedBy: String(_.sample(db.user)._id), time: expTime, creatorId: String(_.sample(db.user)._id), categoryId: category._id, subcategoryId: sub.id});
+							db.expense.push(expense);
 				} else if (nn === 5) {
-					var expense = Factory.build('Expense', {time: expTime, creatorId: String(_.sample(db.user)._id), categoryId: category._id, subcategoryId: sub.id});
+					var expense = Factory.build('Expense', {time: expTime, creatorId: String(_.sample(db.user)._id), categoryId: String(category._id), subcategoryId: String(sub.id)});
 					expense.personal = true;
 					expense.price = casual.integer(300, 700);
 					db.expense.push(expense);
 				}
 				else {
-					var expense = Factory.build('Expense', {time: expTime, creatorId: String(_.sample(db.user)._id), categoryId: category._id, subcategoryId: sub.id});
-					if (expense.currency === 'UAH') {
-						expense.price *= 20;
-					}
-					db.expense.push(expense);
+					_.times(casual.integer(3, 6), function(k) {
+						var expense = Factory.build('Expense', {time: expTime + casual.integer(300, 900) * k, creatorId: String(_.sample(db.user)._id), categoryId: String(category._id), subcategoryId: String(sub.id)});
+						if (expense.currency === 'UAH') {
+							expense.price *= 20;
+						}
+						db.expense.push(expense);
+					});
 				}
 			});
 		});
 	});
 });
 
-var owner = {_id: 'a', login: 'admin@admin.admin', name: 'Admin3000', admin: true, categories: [], 'createdAt': new Date().toISOString(), 'updatedAt': new Date().toISOString(), password: hash};
+var owner = {_id: new ObjectId(), login: 'admin@admin.admin', name: 'Admin9000', admin: true, categories: [], 'createdAt': new Date().toISOString(), 'updatedAt': new Date().toISOString(), password: hash};
 db.user.push(owner);
 
 var url = 'mongodb://localhost:27017/portal-accounting';
