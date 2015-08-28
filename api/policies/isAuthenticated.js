384a03474bfd2e71111b9e44ad19f86b52bebb5f
Alexey Potopakhin <alexey_potopahin@hotmail.com>
2015-08-28 20:00:26 +0300

    Changed port to 1335

--- api/policies/isAuthenticated.js
+++ api/policies/isAuthenticated.js
@@ -2,6 +2,6 @@ module.exports = function(req, res, next) {
 	if (req.user) {
 		return next();
 	} else {
-		return res.redirect('http://localhost:2020/login');
+		return res.redirect('/auth');
 	}
 };
