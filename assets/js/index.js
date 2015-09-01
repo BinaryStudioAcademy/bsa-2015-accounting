17bd4670fb04fe5b31f434df83d2d43b8c0ea977
voby <its.voby@gmail.com>
2015-09-01 20:14:31 +0300

    New header

--- assets/js/index.js
+++ assets/js/index.js
@@ -25,3 +25,21 @@ require('./personal/index.js')(app);
 require('./history/index.js')(app);
 require('./shared/index.js')(app);
 require('./route.js')(app);
+
+var getHeader = function() {
+var request = new XMLHttpRequest();
+request.open('GET', 'http://team.binary-studio.com/app/header', true);
+    request.send();
+    request.onreadystatechange = function() {
+        if (request.readyState != 4) return;
+        if (request.status != 200) {
+            alert(request.status + ': ' + request.statusText);
+        } else {
+           var headerHtml = request.responseText;
+           var headerContainer = document.getElementById('header');
+           headerContainer.innerHTML =headerHtml;
+           headerFunction();
+        }
+    };
+};
+getHeader();
