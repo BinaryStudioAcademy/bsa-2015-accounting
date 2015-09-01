1dcbe8c23e28afa83ed1cfc08a0e1f22f8b2163a
voby <its.voby@gmail.com>
2015-09-01 11:58:58 +0300

    Path fix: works only on prod! v3

--- assets/js/route.js
+++ assets/js/route.js
@@ -6,43 +6,43 @@ module.exports = function(app) {
   function RouteConfig($routeProvider) {
     $routeProvider.
       when('/expenses', {
-        templateUrl: '/templates/Expenses.html',
+        templateUrl: 'templates/Expenses.html',
         controller: 'ExpensesController',
         controllerAs:"expCtr",
         activetab: 'expenses'
       }).
       when('/planning', {
-        templateUrl: '/templates/Budgets.html',
+        templateUrl: 'templates/Budgets.html',
         controller: 'BudgetsController',
         controllerAs: 'budCtrl',
         activetab: 'planning'
       }).
       when('/charts', {
-        templateUrl: '/templates/Charts.html',
+        templateUrl: 'templates/Charts.html',
         controller: 'ChartsController',
         controllerAs:'chartCtrl',
         activetab: 'charts'
       }).
       when('/personal', {
-        templateUrl: '/templates/Personal.html',
+        templateUrl: 'templates/Personal.html',
         controller: 'PersonalController',
         controllerAs:'personalCtrl',
         activetab: 'personal'
       }).
       when('/administration', {
-        templateUrl: '/templates/Administration.html',
+        templateUrl: 'templates/Administration.html',
         controller: 'AdministrationController',
         controllerAs:'admCtrl',
         activetab: 'administration'
       }).
       when('/history', {
-        templateUrl: '/templates/History.html',
+        templateUrl: 'templates/History.html',
         controller: 'HistoryController',
         controllerAs: 'hstCtrl',
         activetab: 'history'
       }).
       when('/bin', {
-        templateUrl: '/templates/Bin.html',
+        templateUrl: 'templates/Bin.html',
         controller: 'BinController',
         controllerAs:'binCtrl',
         activetab: 'bin'
