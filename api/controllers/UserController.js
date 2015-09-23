415f4a546b502459c1450e4bbdc70cbb63e47212
jacobator <jacobator@gmail.com>
2015-09-24 00:13:59 +0300

    testing users 3

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -28,7 +28,7 @@ function getCurrentUser(req, res) {
 function getUsers(req, res) {
 	http.get("http://team.binary-studio.com/profile/api/users/", function(data) {
 		//var users = data.body;
-		var users = JSON.parse(data);
+		var users = data;
 
 		//User.find({deletedBy: {$exists: false}})
 		//	.then(function(localUsers) {
