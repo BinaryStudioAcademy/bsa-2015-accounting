87175bc435c40fb3fd0d2af3de67a389553a0eed
voby <its.voby@gmail.com>
2015-09-24 13:47:53 +0300

    Users api fetch v3

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -29,7 +29,7 @@ function getCurrentUser(req, res) {
 function getUsers(req, res) {
 	var cookies = new Cookies(req, res);
 	console.log('this-is-cookies', cookies.request.headers.Cookie);
-	request.cookie(cookies.request.headers.Cookie)
+	request.cookie(cookies.request.cookies)
 
 	request('http://team.binary-studio.com/profile/api/users/', function (error, response, body) {
 		console.log('this-is-body/users', body);
