760b817f8de9371dc6fba3bd77f091087cc3069f
jacobator <jacobator@gmail.com>
2015-09-30 00:53:22 +0300

    current user tests 3

--- assets/js/users/Users.service.js
+++ assets/js/users/Users.service.js
@@ -37,11 +37,13 @@ module.exports = function(app) {
 		}
 
 		function getCurrentUser() {
-			$resource("user/current").get().$promise.then(function(local_user) {
-				$resource("../profile/api/users?serverUserId=" + local_user.global_id).query().$promise.then(function(global_user) {
-					local_user.name = global_user[0].name + " " + global_user[0].surname;
-					console.log("here is me", local_user);
-					return local_user;
+			return $q(function(resolve, reject) {
+				$resource("user/current").get().$promise.then(function(local_user) {
+					$resource("../profile/api/users?serverUserId=" + local_user.global_id).$promise.query().then(function(global_user) {
+						local_user.name = global_user[0].name + " " + global_user[0].surname;
+						console.log("here is me", local_user);
+						resolve(local_user);
+					});
 				});
 			});
 		}
